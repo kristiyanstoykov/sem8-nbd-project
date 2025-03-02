46fd5a8459aab752f65b7c896543b0679f530e05
kristiyanstoykov <krisistoikov@gmail.com>
2025-03-02 22:12:38 +0200

    Add Mongoose models for Items, Products, and Users

---
+++ my-warehouse-app/src/models/Products.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema(
+  {
+    name: String,
+    price: { type: Number, min: 0 },
+    stock: { type: Number, min: 0 },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Product ??
+  mongoose.model("Product", ProductSchema);
