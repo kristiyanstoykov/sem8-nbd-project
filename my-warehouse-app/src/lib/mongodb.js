b1d94a404ea0b8ef2ea4d9de0966de7ca57430a4
kristiyanstoykov <krisistoikov@gmail.com>
2025-03-02 22:12:32 +0200

    Refactor MongoDB connection to use MongoClient and improve error handling

--- my-warehouse-app/src/lib/mongodb.js
+++ my-warehouse-app/src/lib/mongodb.js
@@ -1,24 +1,30 @@
-import mongoose from 'mongoose';
+import { MongoClient } from "mongodb";
 
-const MONGODB_URI = process.env.MONGODB_URI;
-if (!MONGODB_URI) throw new Error("MONGODB_URI is required.");
+const uri = process.env.MONGODB_URI;
+const options = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+};
 
+let client;
+let clientPromise;
 
-let cached = global.mongoose || { conn: null, promise: null };
-async function dbConnect() {
-
-    if (cached.conn) return cached.conn;
-
-    if (!cached.promise) {
-        cached.promise = mongoose.connect(MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        }).then((mongoose) => mongoose);
-    }
-
-    cached.conn = await cached.promise;
-
-    return cached.conn;
+if (!uri) {
+  throw new Error("Please add your MongoDB URI to .env.local");
 }
 
-export default dbConnect;
+if (process.env.NODE_ENV === "development") {
+  // In development mode, use a global variable so that the value
+  // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  // In production mode, it's best to not use a global variable.
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
+
+export default clientPromise;
