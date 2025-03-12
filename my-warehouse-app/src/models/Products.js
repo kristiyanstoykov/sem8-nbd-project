90b45b43faa96c38bbe10a3f62eab27b8c7c5455
kristiyanstoykov <krisistoikov@gmail.com>
2025-03-12 10:05:49 +0200

    Add thumbnail_guid field to Product schema

--- my-warehouse-app/src/models/Products.js
+++ my-warehouse-app/src/models/Products.js
@@ -5,6 +5,7 @@ const ProductSchema = new mongoose.Schema(
     name: String,
     price: { type: Number, min: 0 },
     stock: { type: Number, min: 0 },
+    thumbnail_guid: { type: String },
   },
   { timestamps: true }
 );
