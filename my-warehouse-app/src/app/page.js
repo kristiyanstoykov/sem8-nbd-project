9a2318bc9b237a898d4879bb5a82e1ec3425f89b
kristiyanstoykov <krisistoikov@gmail.com>
2025-03-17 23:19:46 +0200

    Add user greeting card to Home page for testing logged-in users

--- my-warehouse-app/src/app/page.js
+++ my-warehouse-app/src/app/page.js
@@ -1,11 +1,32 @@
-"use client";
 import Link from "next/link";
 import ProductsCard from "../components/ProductsCard";
+import { getCurrentUser } from "@/auth/nextjs/currentUser";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from "@/components/ui/card";
 
-export default function Home() {
+export default async function Home() {
+  const fullUser = await getCurrentUser();
   return (
-    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center p-8">
-      <ProductsCard />
-    </div>
+    <>
+      {fullUser && (
+        <Card>
+          <CardHeader>
+            <CardTitle>Welcome {fullUser.id}!</CardTitle>
+            <CardDescription>
+              <p>
+                You are currently logged in as <strong>{fullUser.role}</strong>
+              </p>
+            </CardDescription>
+          </CardHeader>
+        </Card>
+      )}
+      <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center p-8">
+        <ProductsCard />
+      </div>
+    </>
   );
 }
