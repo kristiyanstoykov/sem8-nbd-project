ce78e12eb33db1848dd862d44ba2f6af991c5d53
kristiyanstoykov <krisistoikov@gmail.com>
2025-03-23 13:33:07 +0200

    Remove welcome message for logged-in users from Home page

--- my-warehouse-app/src/app/page.js
+++ my-warehouse-app/src/app/page.js
@@ -12,18 +12,6 @@ export default async function Home() {
   const fullUser = await getCurrentUser();
   return (
     <>
-      {fullUser && (
-        <Card>
-          <CardHeader>
-            <CardTitle>Welcome {fullUser.id}!</CardTitle>
-            <CardDescription>
-              <p>
-                You are currently logged in as <strong>{fullUser.role}</strong>
-              </p>
-            </CardDescription>
-          </CardHeader>
-        </Card>
-      )}
       <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center p-8">
         <ProductsCard />
       </div>
